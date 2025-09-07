69ab70ff1b150840daa25542233e81e96e4fa2ad
Leslie Nguyen <gatechme99@users.noreply.github.com>
2025-09-07 22:06:46 +0000

    Set up initial homework format.

---
+++ intermediate-bootcamp/materials/1-dimensional-data-modeling/leslie_notes/dim_model_hw.sql
@@ -0,0 +1,24 @@
+-- Create DDL for an actors table.
+CREATE TYPE films AS (
+	film TEXT,
+	votes INTEGER,
+	rating REAL,
+	filmid TEXT,
+);
+
+CREATE TYPE quality_class AS ENUM (
+    'star', 
+    'good', 
+    'average', 
+    'bad'
+ );
+
+CREATE TABLE actors (
+    actor TEXT,
+    actorid TEXT,
+    films films[],
+    quality_class quality_class,
+    is_active BOOLEAN,
+    current_year INTEGER
+    PRIMARY KEY (actor, actorid)
+)
