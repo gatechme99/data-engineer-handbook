0a5dd14f2b93e506fd246ebc53c0780241104c9a
Leslie Nguyen <gatechme99@users.noreply.github.com>
2025-09-12 21:41:34 +0000

    Continuing lab day 2 for fact modeling.

--- intermediate-bootcamp/materials/2-fact-data-modeling/leslie_fact_notes/fact_model_events.sql
+++ intermediate-bootcamp/materials/2-fact-data-modeling/leslie_fact_notes/fact_model_events.sql
@@ -1,6 +1,6 @@
--- DDL for users_cum
+-- DDL for users_cumulated table.
 CREATE TABLE users_cumulated (
-    user_id BIGINT,
+    user_id TEXT,
     -- The list of dates in the past where the user was active.
     dates_active DATE[],
     -- The current date the user.
@@ -9,6 +9,8 @@ CREATE TABLE users_cumulated (
 );
 
 
+-- Populate users_cumulated table.
+INSERT INTO users_cumulated
 WITH yesterday AS (
     SELECT
         *
@@ -18,7 +20,7 @@ WITH yesterday AS (
 
 today AS (
     SELECT
-        user_id,
+        CAST(user_id AS TEXT) AS user_id,
         DATE(CAST(event_time AS TIMESTAMP)) as date_active
     FROM events
     WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-01')
@@ -28,8 +30,42 @@ today AS (
 
 SELECT
     COALESCE(t.user_id, y.user_id) AS user_id,
-    NULL AS dates_active,
+    CASE 
+        WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
+        WHEN t.date_active IS NULL THEN y.dates_active
+        ELSE ARRAY[t.date_active] || y.dates_active
+    END AS dates_active,
     COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
 FROM today t 
 FULL OUTER JOIN yesterday y
     ON t.user_id = y.user_id
+
+
+WITH users AS(
+    -- List of all the dates user was active.
+    SELECT * FROM users_cumulated
+    WHERE date = DATE('2023-01-31')
+),
+
+series AS (
+    SELECT * 
+    FROM generate_series(DATE('2023-01-01'), DATE('2023-01-31'), INTERVAL '1 day') AS series_date
+),
+
+placeholder_ints AS (
+    SELECT 
+        CASE 
+            WHEN dates_active @> ARRAY [DATE(series_date)]
+            THEN CAST(POW(2, 32 - (date - DATE(series_date))) AS BIGINT)
+            ELSE 0
+        END AS placeholder_int_value, 
+        *
+    FROM users 
+    CROSS JOIN series
+)
+
+SELECT
+    user_id,
+    CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32))
+FROM placeholder_ints
+GROUP BY user_id
