51e8b81b43a55cda0bcd5d0221e073f55c3a3a35
Leslie Nguyen <gatechme99@users.noreply.github.com>
2025-09-16 04:32:15 +0000

    Completed fact modeling homework.

--- intermediate-bootcamp/materials/2-fact-data-modeling/leslie_fact_notes/fact_model_hw.sql
+++ intermediate-bootcamp/materials/2-fact-data-modeling/leslie_fact_notes/fact_model_hw.sql
@@ -39,13 +39,12 @@ WHERE row_num = 1;
 -- DDL for an user_devices_cumulated table.
 CREATE TABLE user_devices_cumulated (
     user_id TEXT,
-    device_id TEXT,
     browser_type TEXT,
-    -- Object to track user active days by browser_type.
+    -- Array to track user active days by browser_type.
     device_activity_datelist DATE[],
     -- The current date.
     date DATE,
-    PRIMARY KEY (user_id, device_id, date)
+    PRIMARY KEY (user_id, browser_type, date)
 );
 
 -- Cumulative query to generate device_activity_datelist from events.
@@ -53,10 +52,9 @@ CREATE TABLE user_devices_cumulated (
 WITH deduped AS (
 	SELECT
 		e.user_id,
-        e.device_id,
-        e.event_time,
         d.browser_type,
-		ROW_NUMBER() OVER (PARTITION BY e.user_id, e.device_id) as row_num
+        e.event_time,
+		ROW_NUMBER() OVER (PARTITION BY e.user_id, d.browser_type, e.event_time) as row_num
 	FROM events e 
     JOIN devices d ON e.device_id = d.device_id
 ),
@@ -70,19 +68,17 @@ yesterday AS (
 today AS (
     SELECT
 	    CAST(user_id AS TEXT) AS user_id,
-       	CAST(device_id AS TEXT) AS device_id,
         CAST(browser_type AS TEXT) AS browser_type,
         DATE(CAST(event_time AS TIMESTAMP)) as date_active
     FROM deduped
     WHERE row_num = 1
 	    AND DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-01')
 	    AND user_id IS NOT NULL
-    GROUP BY user_id, device_id, browser_type, DATE(CAST(event_time AS TIMESTAMP))
+    GROUP BY user_id, browser_type, DATE(CAST(event_time AS TIMESTAMP))
 )
 
 SELECT
     COALESCE(t.user_id, y.user_id) AS user_id,
-    COALESCE(t.device_id, y.device_id) AS device_id,
     COALESCE(t.browser_type, y.browser_type) AS browser_type,
     CASE
         WHEN y.device_activity_datelist IS NULL THEN ARRAY[t.date_active]
@@ -93,7 +89,7 @@ SELECT
 FROM today t
 FULL OUTER JOIN yesterday y
     ON t.user_id = y.user_id
-    AND t.device_id = y.device_id;
+    AND t.browser_type = y.browser_type;
 
 
 -- A datelist_int generation query that converts the 
@@ -120,17 +116,113 @@ CROSS JOIN series;
 
 
 -- DDL for hosts_cumulated table.
-
+CREATE TABLE hosts_cumulated (
+    user_id TEXT,
+    host TEXT,
+    host_activity_datelist DATE[],
+    date DATE,
+    PRIMARY KEY (user_id, host, date)
+);
 
 -- Incremental query to generate host_activity_datelist.
+INSERT INTO hosts_cumulated
+WITH deduped AS (
+	SELECT
+        user_id,
+		host,
+        event_time,
+		ROW_NUMBER() OVER (PARTITION BY host, event_time) as row_num
+	FROM events
+),
+
+yesterday AS (
+    SELECT *
+    FROM hosts_cumulated
+    WHERE date = DATE('2022-12-31')
+),
+
+today AS (
+    SELECT
+        CAST(user_id AS TEXT) AS user_id,
+	    host,
+        DATE(CAST(event_time AS TIMESTAMP)) as date_active
+    FROM deduped
+    WHERE row_num = 1
+	    AND DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-01')
+	    AND user_id IS NOT NULL
+    GROUP BY user_id, host, DATE(CAST(event_time AS TIMESTAMP))
+)
+
+SELECT
+    COALESCE(t.user_id, y.user_id) AS user_id,
+    COALESCE(t.host, y.host) AS host,
+    CASE
+        WHEN y.host_activity_datelist IS NULL THEN ARRAY[t.date_active]
+        WHEN t.date_active IS NULL THEN y.host_activity_datelist
+		ELSE ARRAY[t.date_active] || y.host_activity_datelist
+    END AS host_activity_datelist,
+    COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
+FROM today t
+FULL OUTER JOIN yesterday y
+    ON t.user_id = y.user_id
+        AND t.host = y.host;
 
 
 -- Monthly, reduced fact table DDL host_activity_reduced.
 CREATE TABLE host_activity_reduced (
-    host TEXT
+    user_id NUMERIC,
+    host TEXT,
     month_start DATE,
-    PRIMARY KEY (host, month_start)
-)
+    first_metric_name TEXT, -- daily_hits
+    hit_array REAL[], -- COUNT(1)
+    second_metric_name TEXT, -- daily_unique_hits
+    unique_visitors REAL[], -- COUNT(DISTINCT user_id)
+    PRIMARY KEY (user_id, host, month_start, first_metric_name, second_metric_name)
+);
+
 
 -- Incremental query that loads host_activity_reduced.
+INSERT INTO host_activity_reduced
+WITH daily_aggregate AS (
+    SELECT
+        user_id,
+        host,
+        DATE(event_time) AS date,
+        COUNT(1) AS num_daily_hits,
+        COUNT(DISTINCT user_id) AS num_daily_uniques
+    FROM events
+    WHERE DATE(event_time) = DATE('2023-01-01')
+        AND user_id IS NOT NULL
+    GROUP BY user_id, host, DATE(event_time)
+),
 
+yesterday_array AS (
+    SELECT *
+    FROM host_activity_reduced
+    WHERE month_start = DATE('2023-01-01')
+)
+
+SELECT 
+    COALESCE(da.user_id, ya.user_id) AS user_id,
+    COALESCE(da.host, ya.host) AS host,
+    COALESCE(ya.month_start, DATE_TRUNC('month', da.date)) AS month_start,
+    'daily_hits' AS first_metric_name,
+    CASE 
+        WHEN ya.hit_array IS NOT NULL THEN 
+            ya.hit_array || ARRAY[COALESCE(da.num_daily_hits, 0)]
+        WHEN ya.hit_array IS NULL THEN
+            ARRAY_FILL(0, ARRAY[COALESCE(da.date - DATE(DATE_TRUNC('month', da.date)), 0)]) || ARRAY[COALESCE(da.num_daily_hits, 0)]
+    END AS hit_array,
+    'daily_unique_hits' AS second_metric_name,
+    CASE 
+        WHEN ya.unique_visitors IS NOT NULL THEN 
+            ya.unique_visitors || ARRAY[COALESCE(da.num_daily_uniques, 0)]
+        WHEN ya.unique_visitors IS NULL THEN
+            ARRAY_FILL(0, ARRAY[COALESCE(da.date - DATE(DATE_TRUNC('month', da.date)), 0)]) || ARRAY[COALESCE(da.num_daily_uniques, 0)]
+    END AS unique_visitors
+FROM daily_aggregate da 
+FULL OUTER JOIN yesterday_array ya
+    ON da.user_id = ya.user_id
+    AND da.host = ya.host
+ON CONFLICT(user_id, host, month_start, first_metric_name, second_metric_name)
+DO UPDATE SET hit_array = EXCLUDED.hit_array, unique_visitors = EXCLUDED.unique_visitors;
